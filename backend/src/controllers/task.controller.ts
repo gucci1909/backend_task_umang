853f16617499ad1a33581969f031ddeb4206b3b3
gucci1909 <umangarora0134@gmail.com>
2023-02-08 20:33:44 +0530

    controllers change

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Task_Model from "../models/task.models.js";
 import client from "../config/redis.js";
 
-// controller of /fetchAllTasks route , here we are getting both databases of redis and mongodb . We are taking redis database as a prior one because first 51 todo's will be added to redis.
+// controller of /fetchAllTasks route , here we are getting both databases of redis and mongodb . We are taking mongodb database as a prior one because first 51 todo's will be added to redis and then moved to mongodb.
 const task_controller = async (req: Request, res: Response) => {
   try {
     let redis_data: any = await client.get("BACKEND_TASK_UMANG");
